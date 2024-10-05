df1768bcdaf49ad175275fb477b18015549562fe
aman <ak6255456@gmail.com>
2024-10-05 12:14:02 +0530

    crwn-clothing completed

--- src/routes/home/Navigation/Navigation.components.tsx
+++ src/routes/home/Navigation/Navigation.components.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
 import CardIcon from "../../../components/card-icon/card-icon.components";
 import { UserContext } from "../../../components/contexts/user.context";
